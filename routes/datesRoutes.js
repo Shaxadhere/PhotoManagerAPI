f0c36956adb52a0fabe096236c06ada9170c83b5
Shaxadhere <Shaxad.here@gmail.com>
2021-04-27 13:45:59 +0500

    delete date -d

--- routes/datesRoutes.js
+++ routes/datesRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {List, Create, DateById, Read} = require('../controllers/datesController')
+const {List, Create, DateById, Read, Remove, Update} = require('../controllers/datesController')
 
 router.get('/dates', List)
 router.get('/dates/:dateId', DateById, Read)
 router.post('/dates/create', Create)
 router.put('/dates/:dateId', DateById, Update)
-router.delete('/delete/:dateId', DateById, Delete)
+router.delete('/dates/:dateId', DateById, Remove)
 
 router.param('dateId', DateById)
 module.exports = router

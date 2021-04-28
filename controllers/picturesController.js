2423d526f68a375ac7d4f5f3f8b69abd805dd526
Shaxadhere <Shaxad.here@gmail.com>
2021-04-28 11:23:52 +0500

    picture controller create

--- controllers/picturesController.js
+++ controllers/picturesController.js
@@ -1,3 +1,6 @@
+const formidable = require('formidable')
+const lodash = require('lodash')
+const fs = require('fs')
 const Picture = require('../models/picturesModel')
 
 exports.PictureById = (req, res, next) => {
@@ -18,7 +21,24 @@ exports.List = (req, res) => {
 }
 
 exports.Create = (req, res) => {
-
+    let form = new formidable.IncomingForm()
+    form.keepExtentions = true
+    form.parse(req, (err, fields, files) => {
+        if(err){
+            return res.status(500).json({message: "Image could not be uploaded"})
+        }
+        let picture = new Picture(fields)
+        if(files.photo){
+            picture.photo.data = fs.readFileSync(files.photo.path)
+            picture.photo.contentType = files.photo.type
+        }
+        picture.save((err, result) => {
+            if(err){
+                return res.status(400).json({message: "Picture could not be added"})
+            }
+            res.status(200).json({Picture: result})
+        })
+    })
 }
 
 exports.Remove = (req, res) => {

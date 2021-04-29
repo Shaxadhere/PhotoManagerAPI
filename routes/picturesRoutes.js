944cccb8c3252002d6cec82da834967ffd7ce66c
Shaxadhere <Shaxad.here@gmail.com>
2021-04-29 11:04:23 +0500

    remove picture

--- routes/picturesRoutes.js
+++ routes/picturesRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {List, Create, PictureById, Read, Update} = require('../controllers/picturesController')
+const {List, Create, PictureById, Read, Update, Remove} = require('../controllers/picturesController')
 
 router.get('/pictures/:pictureId', Read)
 router.get('/pictures', List)
 router.post('/pictures/create', Create)
-router.put('/pictures/update/:pictureId', Update)
+router.put('/pictures/:pictureId', Update)
+router.delete('/pictures/:pictureId', Remove)
 
 router.param('pictureId', PictureById)
 module.exports = router
